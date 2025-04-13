774b647a8fedce3de7329e3d6ad7617c9b604dae
eggerino <eggensberger.armin@googlemail.com>
2025-04-13 15:52:36 +0200

    initial project setup

---
+++ include/boii/common.h
@@ -0,0 +1,14 @@
+#ifndef BOII_COMMON_H_
+#define BOII_COMMON_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#define ERR_OK 0
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "boii/common.h"
+
+int main(int argc, char **argv) {
+    printf("Hello World\n");
+    return ERR_OK;
+}
