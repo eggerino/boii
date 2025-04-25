02536bccbf2180c8cf26f1039663f760ffd0c662
eggerino <eggensberger.armin@googlemail.com>
2025-04-26 00:16:58 +0200

    isolated instructions to test no more setup instructions needed

--- Boii.Test/Processing/CpuTest.cs
+++ Boii.Test/Processing/CpuTest.cs
@@ -56,19 +56,16 @@ public class CpuTest
     public void LoadFromA()
     {
         var bus = Bus.From([
-            0b0011_1110, 0xFF,  // ld a, 256
-            0b0000_1110, 0x05,  // ld c, 5
-            0b0001_1110, 0x06,  // ld e, 6
             0b0000_0010,        // ld [bc], a
             0b0001_0010,        // ld [de], a
             0b0010_0010,        // ld [hl+], a
             0b0011_0010,        // ld [hl-], a
         ]);
-        var cpu = Cpu.Create(bus);
+        var cpu = Cpu.CreateWithRegisterState(bus, new(0xFF00, 0x0005, 0x0006, 0, 0, 0x0100));
 
-        Step(cpu, 7);
+        Step(cpu, 4);
 
-        AssertCpu(14, new(0xFF00, 0x0005, 0x0006, 0x0000, 0, 0x010A), cpu);
+        AssertCpu(8, new(0xFF00, 0x0005, 0x0006, 0x0000, 0, 0x0104), cpu);
         Assert.Equal(0xFF, bus.Read(0x0005));
         Assert.Equal(0xFF, bus.Read(0x0006));
         Assert.Equal(0xFF, bus.Read(0x0000));
@@ -79,9 +76,6 @@ public class CpuTest
     public void LoadIntoA()
     {
         var bus = Bus.From([
-            0b0000_0001, 0x01, 0x00,    // ld bc, 1
-            0b0001_0001, 0x02, 0x00,    // ld de, 2
-            0b0010_0001, 0x03, 0x00,    // ld hl, 3
             0b0000_1010,                // ld a, [bc]
             0b0001_1010,                // ld a, [de]
             0b0010_1010,                // ld a, [hl+]
@@ -92,30 +86,29 @@ public class CpuTest
         bus.Write(3, 3);
         bus.Write(4, 4);
 
-        var cpu = Cpu.Create(bus);
+        var cpu = Cpu.CreateWithRegisterState(bus, new(0, 1, 2, 3, 0, 0x0100));
 
-        Step(cpu, 4); ;
-        AssertCpu(11, new(0x0100, 0x0001, 0x0002, 0x0003, 0, 0x010A), cpu);
         cpu.Step();
-        AssertCpu(13, new(0x0200, 0x0001, 0x0002, 0x0003, 0, 0x010B), cpu);
+        AssertCpu(2, new(0x0100, 0x0001, 0x0002, 0x0003, 0, 0x0101), cpu);
         cpu.Step();
-        AssertCpu(15, new(0x0300, 0x0001, 0x0002, 0x0004, 0, 0x010C), cpu);
+        AssertCpu(4, new(0x0200, 0x0001, 0x0002, 0x0003, 0, 0x0102), cpu);
         cpu.Step();
-        AssertCpu(17, new(0x0400, 0x0001, 0x0002, 0x0003, 0, 0x010D), cpu);
+        AssertCpu(6, new(0x0300, 0x0001, 0x0002, 0x0004, 0, 0x0103), cpu);
+        cpu.Step();
+        AssertCpu(8, new(0x0400, 0x0001, 0x0002, 0x0003, 0, 0x0104), cpu);
     }
 
     [Fact]
     public void LoadFromStackPointer()
     {
         var bus = Bus.From([
-            0b0011_0001, 0x07, 0x08,    // ld sp, 0x0807
             0b0000_1000, 0x01, 0x00     // ld [0x0001], sp
         ]);
-        var cpu = Cpu.Create(bus);
+        var cpu = Cpu.CreateWithRegisterState(bus, new(0, 0, 0, 0, 0x0807, 0x0100));
 
-        Step(cpu, 2);
+        cpu.Step();
 
-        AssertCpu(8, new(0, 0, 0, 0, 0x0807, 0x0106), cpu);
+        AssertCpu(5, new(0, 0, 0, 0, 0x0807, 0x0103), cpu);
         Assert.Equal(0x07, bus.Read(1));
         Assert.Equal(0x08, bus.Read(2));
     }
@@ -156,32 +149,27 @@ public class CpuTest
     public void AddRegister16ToHL()
     {
         var bus = Bus.From([
-            0b0000_0001, 0xFF, 0x0F,    // ld bc, 0x0FFF
-            0b0001_0001, 0x01, 0x00,    // ld de, 0x0001
-            0b0011_0001, 0x01, 0xE0,    // ld sp, 0xE001
             0b0000_1001,                // dec bc
             0b0001_1001,                // dec de
             0b0010_1001,                // dec hl
             0b0011_1001,                // dec sp
         ]);
-        var cpu = Cpu.Create(bus);
+        var cpu = Cpu.CreateWithRegisterState(bus, new(0, 0x0FFF, 0x0001, 0, 0xE001, 0x0100));
 
-        Step(cpu, 4);
-        AssertCpu(11, new(0b0000_0000_0000_0000, 0x0FFF, 0x0001, 0x0FFF, 0xE001, 0x010A), cpu);
         cpu.Step();
-        AssertCpu(13, new(0b0000_0000_0010_0000, 0x0FFF, 0x0001, 0x1000, 0xE001, 0x010B), cpu);
+        AssertCpu(2, new(0b0000_0000_0000_0000, 0x0FFF, 0x0001, 0x0FFF, 0xE001, 0x0101), cpu);
         cpu.Step();
-        AssertCpu(15, new(0b0000_0000_0010_0000, 0x0FFF, 0x0001, 0x2000, 0xE001, 0x010C), cpu);
+        AssertCpu(4, new(0b0000_0000_0010_0000, 0x0FFF, 0x0001, 0x1000, 0xE001, 0x0102), cpu);
         cpu.Step();
-        AssertCpu(17, new(0b0000_0000_0011_0000, 0x0FFF, 0x0001, 0x0001, 0xE001, 0x010D), cpu);
+        AssertCpu(6, new(0b0000_0000_0010_0000, 0x0FFF, 0x0001, 0x2000, 0xE001, 0x0103), cpu);
+        cpu.Step();
+        AssertCpu(8, new(0b0000_0000_0011_0000, 0x0FFF, 0x0001, 0x0001, 0xE001, 0x0104), cpu);
     }
 
     [Fact]
     public void IncrementRegister8()
     {
         var bus = Bus.From([
-            0b0000_0110, 0xFF,  // ld b, 255
-            0b0000_1110, 0x0F,  // ld c, 16
             0b0000_0100,        // inc b
             0b0000_1100,        // inc c
             0b0001_0100,        // inc d
@@ -191,11 +179,11 @@ public class CpuTest
             0b0010_1100,        // inc l
             0b0011_1100,        // inc a
         ]);
-        var cpu = Cpu.Create(bus);
+        var cpu = Cpu.CreateWithRegisterState(bus, new(0, 0xFF0F, 0, 0, 0, 0x0100));
 
-        Step(cpu, 10);
+        Step(cpu, 8);
 
-        AssertCpu(14, new(0x0100 | 0b1010_0000, 0x0010, 0x0101, 0x0101, 0x0000, 0x010C), cpu);
+        AssertCpu(10, new(0x0100 | 0b1010_0000, 0x0010, 0x0101, 0x0101, 0x0000, 0x0108), cpu);
         Assert.Equal(0x01, bus.Read(0x0000));
     }
 
@@ -203,7 +191,6 @@ public class CpuTest
     public void DecrementRegister8()
     {
         var bus = Bus.From([
-            0b0000_0110, 0x01,  // ld b, 1
             0b0000_0101,        // dec b
             0b0000_1101,        // dec c
             0b0001_0101,        // dec d
@@ -213,11 +200,11 @@ public class CpuTest
             0b0010_1101,        // dec l
             0b0011_1101,        // dec a
         ]);
-        var cpu = Cpu.Create(bus);
+        var cpu = Cpu.CreateWithRegisterState(bus, new(0, 0x0100, 0, 0, 0, 0x0100));
 
-        Step(cpu, 9);
+        Step(cpu, 8);
 
-        AssertCpu(12, new(0xFF00 | 0b1110_0000, 0x00FF, 0xFFFF, 0xFFFF, 0x0000, 0x010A), cpu);
+        AssertCpu(10, new(0xFF00 | 0b1110_0000, 0x00FF, 0xFFFF, 0xFFFF, 0x0000, 0x0108), cpu);
         Assert.Equal(0xFF, bus.Read(0x0000));
     }
 
@@ -225,32 +212,30 @@ public class CpuTest
     public void RotateLeftA_WithAndWithoutCarry()
     {
         var bus = Bus.From([
-            0b0011_1110, 0b1000_1110,   // ld a, 0b1000_1110
             0b0000_0111,                // rlca
             0b0001_0111                 // rla
         ]);
-        var cpu = Cpu.Create(bus);
+        var cpu = Cpu.CreateWithRegisterState(bus, new(0b1000_1110_0000_0000, 0, 0, 0, 0, 0x0100));
 
-        Step(cpu, 2);
-        AssertCpu(3, new(0b0001_1101_0001_0000, 0, 0, 0, 0, 0x0103), cpu);
         cpu.Step();
-        AssertCpu(4, new(0b0011_1011_0000_0000, 0, 0, 0, 0, 0x0104), cpu);
+        AssertCpu(1, new(0b0001_1101_0001_0000, 0, 0, 0, 0, 0x0101), cpu);
+        cpu.Step();
+        AssertCpu(2, new(0b0011_1011_0000_0000, 0, 0, 0, 0, 0x0102), cpu);
     }
 
     [Fact]
     public void RotateRightA_WithAndWithoutCarry()
     {
         var bus = Bus.From([
-            0b0011_1110, 0b0111_0001,   // ld a, 0b0111_0001
             0b0000_1111,                // rrca
             0b0001_1111                 // rra
         ]);
-        var cpu = Cpu.Create(bus);
+        var cpu = Cpu.CreateWithRegisterState(bus, new(0b0111_0001_0000_0000, 0, 0, 0, 0, 0x0100));
 
-        Step(cpu, 2);
-        AssertCpu(3, new(0b1011_1000_0001_0000, 0, 0, 0, 0, 0x0103), cpu);
         cpu.Step();
-        AssertCpu(4, new(0b1101_1100_0000_0000, 0, 0, 0, 0, 0x0104), cpu);
+        AssertCpu(1, new(0b1011_1000_0001_0000, 0, 0, 0, 0, 0x0101), cpu);
+        cpu.Step();
+        AssertCpu(2, new(0b1101_1100_0000_0000, 0, 0, 0, 0, 0x0102), cpu);
     }
 
     [Theory]
