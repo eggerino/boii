b2256b2052409fd9ddc2a881cc1467f7a10f3454
eggerino <eggensberger.armin@googlemail.com>
2025-04-26 20:14:17 +0200

    improved error message for debugging

--- Boii/Errors/InvalidOpcode.cs
+++ Boii/Errors/InvalidOpcode.cs
@@ -7,7 +7,7 @@ public class InvalidOpcode : Exception
     public byte Opcode { get; }
     public ushort Address { get; }
 
-    private InvalidOpcode(byte opcode, ushort address) : base($"Invalid opcode of {opcode} at 0x{address:X}") =>
+    private InvalidOpcode(byte opcode, ushort address) : base($"Invalid opcode of {opcode}(0x{opcode:X}) at 0x{address:X}") =>
         (Opcode, Address) = (opcode, address);
 
     public static InvalidOpcode Create(byte opcode, ushort address) => new(opcode, address);
