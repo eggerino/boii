4a102e06e6a322b808a8a7b05145fef5ccf9fd47
eggerino <eggensberger.armin@googlemail.com>
2026-07-31 20:02:35 +0000

    add invalid instruction since 11 bytes are not defined

--- src/cpu/instr.rs
+++ src/cpu/instr.rs
@@ -1,151 +1,166 @@
-#[derive(Copy, Clone)]
-pub enum Register8 {
-    B = 0,
-    C = 1,
-    D = 2,
-    E = 3,
-    H = 4,
-    L = 5,
-    HLAsPointer = 6,
-    A = 7,
+enum U2 {
+    Zero,
+    One,
+    Two,
+    Three,
 }
 
-#[derive(Copy, Clone)]
-pub enum Register16 {
-    BC = 0,
-    DE = 1,
-    HL = 2,
-    StackPointer = 3,
-}
-
-#[derive(Copy, Clone)]
-pub enum Register16Stack {
-    BC = 0,
-    DE = 1,
-    HL = 2,
-    AF = 3,
-}
-
-#[derive(Copy, Clone)]
-pub enum Register16Memory {
-    BC = 0,
-    DE = 1,
-    HLInc = 2,
-    HLDec = 3,
-}
-
-#[derive(Copy, Clone)]
-pub enum Condition {
-    NotZero = 0,
-    Zero = 1,
-    NotCarry = 2,
-    Carry = 3,
+impl U2 {
+    fn from(x: u8, offset: u8) -> Self {
+        let mask = 0b11;
+        match (x >> offset) & mask {
+            0 => Self::Zero,
+            1 => Self::One,
+            2 => Self::Two,
+            3 => Self::Three,
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[derive(Copy, Clone)]
 pub enum U3 {
-    Zero = 0,
-    One = 1,
-    Two = 2,
-    Three = 3,
-    Four = 4,
-    Five = 5,
-    Six = 6,
-    Seven = 7,
-}
-
-#[inline]
-fn slice(x: u8, offset: u8, length: u8) -> u8 {
-    let mask = !((0xFF as u8) << length);
-    (x >> offset) & mask
-}
-
-#[inline]
-pub fn r8(x: u8, offset: u8) -> Register8 {
-    match slice(x, offset, 3) {
-        0 => Register8::B,
-        1 => Register8::C,
-        2 => Register8::D,
-        3 => Register8::E,
-        4 => Register8::H,
-        5 => Register8::L,
-        6 => Register8::HLAsPointer,
-        7 => Register8::A,
-        _ => unreachable!(),
-    }
-}
-
-#[inline]
-pub fn r16(x: u8, offset: u8) -> Register16 {
-    match slice(x, offset, 2) {
-        0 => Register16::BC,
-        1 => Register16::DE,
-        2 => Register16::HL,
-        3 => Register16::StackPointer,
-        _ => unreachable!(),
-    }
-}
-
-#[inline]
-pub fn r16stk(x: u8, offset: u8) -> Register16Stack {
-    match slice(x, offset, 2) {
-        0 => Register16Stack::BC,
-        1 => Register16Stack::DE,
-        2 => Register16Stack::HL,
-        3 => Register16Stack::AF,
-        _ => unreachable!(),
-    }
-}
-
-#[inline]
-pub fn r16mem(x: u8, offset: u8) -> Register16Memory {
-    match slice(x, offset, 2) {
-        0 => Register16Memory::BC,
-        1 => Register16Memory::DE,
-        2 => Register16Memory::HLInc,
-        3 => Register16Memory::HLDec,
-        _ => unreachable!(),
-    }
-}
-
-#[inline]
-pub fn cond(x: u8, offset: u8) -> Condition {
-    match slice(x, offset, 2) {
-        0 => Condition::NotZero,
-        1 => Condition::Zero,
-        2 => Condition::NotCarry,
-        3 => Condition::Carry,
-        _ => unreachable!(),
-    }
-}
-
-#[inline]
-pub fn u3(x: u8, offset: u8) -> U3 {
-    match slice(x, offset, 2) {
-        0 => U3::Zero,
-        1 => U3::One,
-        2 => U3::Two,
-        3 => U3::Three,
-        4 => U3::Four,
-        5 => U3::Five,
-        6 => U3::Six,
-        7 => U3::Seven,
-        _ => unreachable!(),
-    }
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
 }
 
 impl U3 {
-    #[inline]
-    pub fn to_u8(self) -> u8 {
+    fn from(x: u8, offset: u8) -> Self {
+        let mask = 0b111;
+        match (x >> offset) & mask {
+            0 => Self::Zero,
+            1 => Self::One,
+            2 => Self::Two,
+            3 => Self::Three,
+            4 => Self::Four,
+            5 => Self::Five,
+            6 => Self::Six,
+            7 => Self::Seven,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Into<u8> for U3 {
+    fn into(self) -> u8 {
         match self {
-            U3::Zero => 0,
-            U3::One => 1,
-            U3::Two => 2,
-            U3::Three => 3,
-            U3::Four => 4,
-            U3::Five => 5,
-            U3::Six => 6,
-            U3::Seven => 7,
+            Self::Zero => 0,
+            Self::One => 1,
+            Self::Two => 2,
+            Self::Three => 3,
+            Self::Four => 4,
+            Self::Five => 5,
+            Self::Six => 6,
+            Self::Seven => 7,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Register8 {
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+    HLAsPointer,
+    A,
+}
+
+impl Register8 {
+    fn from(x: u8, offset: u8) -> Self {
+        match U3::from(x, offset) {
+            U3::Zero => Self::B,
+            U3::One => Self::C,
+            U3::Two => Self::D,
+            U3::Three => Self::E,
+            U3::Four => Self::H,
+            U3::Five => Self::L,
+            U3::Six => Self::HLAsPointer,
+            U3::Seven => Self::A,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Register16 {
+    BC,
+    DE,
+    HL,
+    StackPointer,
+}
+
+impl Register16 {
+    fn from(x: u8, offset: u8) -> Self {
+        match U2::from(x, offset) {
+            U2::Zero => Self::BC,
+            U2::One => Self::DE,
+            U2::Two => Self::HL,
+            U2::Three => Self::StackPointer,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Register16Stack {
+    BC,
+    DE,
+    HL,
+    AF,
+}
+
+impl Register16Stack {
+    fn from(x: u8, offset: u8) -> Self {
+        match U2::from(x, offset) {
+            U2::Zero => Self::BC,
+            U2::One => Self::DE,
+            U2::Two => Self::HL,
+            U2::Three => Self::AF,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Register16Memory {
+    BC,
+    DE,
+    HLInc,
+    HLDec,
+}
+
+impl Register16Memory {
+    fn from(x: u8, offset: u8) -> Self {
+        match U2::from(x, offset) {
+            U2::Zero => Self::BC,
+            U2::One => Self::DE,
+            U2::Two => Self::HLInc,
+            U2::Three => Self::HLDec,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Condition {
+    NotZero,
+    Zero,
+    NotCarry,
+    Carry,
+}
+
+impl Condition {
+    fn from(x: u8, offset: u8) -> Self {
+        match U2::from(x, offset) {
+            U2::Zero => Self::NotZero,
+            U2::One => Self::Zero,
+            U2::Two => Self::NotCarry,
+            U2::Three => Self::Carry,
         }
     }
 }
@@ -235,11 +250,13 @@ pub enum Instruction {
 
     // 16 Bit instructions
     Prefixed,
+
+    Invalid,
 }
 
 impl Instruction {
-    pub fn from_opcode(opcode: u8) -> Option<Self> {
-        let inst = match opcode {
+    pub fn from(opcode: u8) -> Self {
+        match opcode {
             // Misc
             0x00 => Self::Nop,
             0b0001_0000 => Self::Stop,
@@ -251,47 +268,59 @@ impl Instruction {
             0b1111_0011 => Self::DisableInterrupt,
 
             // Load
-            x if (x & 0b1100_0111) == 0b0000_0110 => Self::LoadLiteral8(r8(x, 3)),
+            x if (x & 0b1100_0111) == 0b0000_0110 => Self::LoadLiteral8(Register8::from(x, 3)),
             x if (x & 0b1100_0000) == 0b0100_0000 => Self::LoadRegister8 {
-                src: r8(x, 0),
-                dest: r8(x, 3),
+                src: Register8::from(x, 0),
+                dest: Register8::from(x, 3),
             }, // Must be after halt, halt has same bit pattern as ld [hl], [hl]
-            x if (x & 0b1100_1111) == 0b0000_0001 => Self::LoadLiteral16(r16(x, 4)),
-            x if (x & 0b1100_1111) == 0b0000_0010 => Self::LoadFromA(r16mem(x, 4)),
+            x if (x & 0b1100_1111) == 0b0000_0001 => Self::LoadLiteral16(Register16::from(x, 4)),
+            x if (x & 0b1100_1111) == 0b0000_0010 => Self::LoadFromA(Register16Memory::from(x, 4)),
             0b1110_1010 => Self::LoadFromAIntoLiteral16Pointer,
             0b1110_0000 => Self::LoadFromAIntoLiteral8HighPointer,
             0b1110_0010 => Self::LoadFromAIntoCHighPointer,
-            x if (x & 0b1100_1111) == 0b0000_1010 => Self::LoadIntoA(r16mem(x, 4)),
+            x if (x & 0b1100_1111) == 0b0000_1010 => Self::LoadIntoA(Register16Memory::from(x, 4)),
             0b1111_1010 => Self::LoadFromLiteral16PointerIntoA,
             0b1111_0000 => Self::LoadFromLiteral8HighPointerIntoA,
             0b1111_0010 => Self::LoadFromCHighPointerIntoA,
 
             // 8 Bit arithmetic
-            x if (x & 0b1100_0111) == 0b0000_0100 => Self::IncrementRegister8(r8(x, 3)),
-            x if (x & 0b1100_0111) == 0b0000_0101 => Self::DecrementRegister8(r8(x, 3)),
-            x if (x & 0b1111_1000) == 0b1000_0000 => Self::AddToA(r8(x, 0)),
+            x if (x & 0b1100_0111) == 0b0000_0100 => {
+                Self::IncrementRegister8(Register8::from(x, 3))
+            }
+            x if (x & 0b1100_0111) == 0b0000_0101 => {
+                Self::DecrementRegister8(Register8::from(x, 3))
+            }
+            x if (x & 0b1111_1000) == 0b1000_0000 => Self::AddToA(Register8::from(x, 0)),
             0b1100_0110 => Self::AddLiteral8ToA,
-            x if (x & 0b1111_1000) == 0b1000_1000 => Self::AddToACarry(r8(x, 0)),
+            x if (x & 0b1111_1000) == 0b1000_1000 => Self::AddToACarry(Register8::from(x, 0)),
             0b1100_1110 => Self::AddLiteral8ToACarry,
-            x if (x & 0b1111_1000) == 0b1001_0000 => Self::SubtractFromA(r8(x, 0)),
+            x if (x & 0b1111_1000) == 0b1001_0000 => Self::SubtractFromA(Register8::from(x, 0)),
             0b1101_0110 => Self::SubtractLiteral8FromA,
-            x if (x & 0b1111_1000) == 0b1001_1000 => Self::SubtractFromACarry(r8(x, 0)),
+            x if (x & 0b1111_1000) == 0b1001_1000 => {
+                Self::SubtractFromACarry(Register8::from(x, 0))
+            }
             0b1101_1110 => Self::SubtractLiteral8FromACarry,
-            x if (x & 0b1111_1000) == 0b1011_1000 => Self::CompareToA(r8(x, 0)),
+            x if (x & 0b1111_1000) == 0b1011_1000 => Self::CompareToA(Register8::from(x, 0)),
             0b1111_1110 => Self::CompareLiteral8ToA,
 
             // 16 Bit arithmetic
-            x if (x & 0b1100_1111) == 0b0000_0011 => Self::IncrementRegister16(r16(x, 4)),
-            x if (x & 0b1100_1111) == 0b0000_1011 => Self::DecrementRegister16(r16(x, 4)),
-            x if (x & 0b1100_1111) == 0b0000_1001 => Self::AddRegister16ToHL(r16(x, 4)),
+            x if (x & 0b1100_1111) == 0b0000_0011 => {
+                Self::IncrementRegister16(Register16::from(x, 4))
+            }
+            x if (x & 0b1100_1111) == 0b0000_1011 => {
+                Self::DecrementRegister16(Register16::from(x, 4))
+            }
+            x if (x & 0b1100_1111) == 0b0000_1001 => {
+                Self::AddRegister16ToHL(Register16::from(x, 4))
+            }
 
             // Bitwise logic
             0b0010_1111 => Self::ComplementA,
-            x if (x & 0b1111_1000) == 0b1010_0000 => Self::AndWithA(r8(x, 0)),
+            x if (x & 0b1111_1000) == 0b1010_0000 => Self::AndWithA(Register8::from(x, 0)),
             0b1110_0110 => Self::AndLiteral8WithA,
-            x if (x & 0b1111_1000) == 0b1010_1000 => Self::XorWithA(r8(x, 0)),
+            x if (x & 0b1111_1000) == 0b1010_1000 => Self::XorWithA(Register8::from(x, 0)),
             0b1110_1110 => Self::XorLiteral8WithA,
-            x if (x & 0b1111_1000) == 0b1011_0000 => Self::OrWithA(r8(x, 0)),
+            x if (x & 0b1111_1000) == 0b1011_0000 => Self::OrWithA(Register8::from(x, 0)),
             0b1111_0110 => Self::OrLiteral8WithA,
 
             // Bit shift
@@ -302,15 +331,17 @@ impl Instruction {
 
             // Jump and subroutine
             0b0001_1000 => Self::JumpRelative,
-            x if (x & 0b1110_0111) == 0b0010_0000 => Self::ConditionalJumpRelative(cond(x, 3)),
+            x if (x & 0b1110_0111) == 0b0010_0000 => {
+                Self::ConditionalJumpRelative(Condition::from(x, 3))
+            }
             0b1100_0011 => Self::Jump,
-            x if (x & 0b1110_0111) == 0b1100_0010 => Self::ConditionalJump(cond(x, 3)),
+            x if (x & 0b1110_0111) == 0b1100_0010 => Self::ConditionalJump(Condition::from(x, 3)),
             0b1110_1001 => Self::JumpHL,
             0b1100_1101 => Self::Call,
-            x if (x & 0b1110_0111) == 0b1100_0100 => Self::ConditionalCall(cond(x, 3)),
-            x if (x & 0b1100_0111) == 0b1100_0111 => Self::Restart(u3(x, 3)),
+            x if (x & 0b1110_0111) == 0b1100_0100 => Self::ConditionalCall(Condition::from(x, 3)),
+            x if (x & 0b1100_0111) == 0b1100_0111 => Self::Restart(U3::from(x, 3)),
             0b1100_1001 => Self::Return,
-            x if (x & 0b1110_0111) == 0b1100_0000 => Self::ConditionalReturn(cond(x, 3)),
+            x if (x & 0b1110_0111) == 0b1100_0000 => Self::ConditionalReturn(Condition::from(x, 3)),
             0b1101_1001 => Self::ReturnInterrupt,
 
             // Carry flag
@@ -318,8 +349,8 @@ impl Instruction {
             0b0011_1111 => Self::ComplementCarryFlag,
 
             // Stack manipulation
-            x if (x & 0b1100_1111) == 0b1100_0101 => Self::Push(r16stk(x, 4)),
-            x if (x & 0b1100_1111) == 0b1100_0001 => Self::Pop(r16stk(x, 4)),
+            x if (x & 0b1100_1111) == 0b1100_0101 => Self::Push(Register16Stack::from(x, 4)),
+            x if (x & 0b1100_1111) == 0b1100_0001 => Self::Pop(Register16Stack::from(x, 4)),
             0b1110_1000 => Self::AddSignedLiteral8ToStackPointer,
             0b0000_1000 => Self::LoadFromStackPointerIntoLiteral16Pointer,
             0b1111_1000 => Self::LoadFromStackPointerPlusSignedLiteral8IntoHL,
@@ -328,9 +359,12 @@ impl Instruction {
             // 16 Bit instructions
             0xCB => Self::Prefixed,
 
-            _ => None?,
-        };
-        Some(inst)
+            0xD3 | 0xE3 | 0xE4 | 0xF4 | 0xDB | 0xEB | 0xEC | 0xFC | 0xDD | 0xED | 0xFD => {
+                Self::Invalid
+            }
+
+            _ => unreachable!(),
+        }
     }
 }
 
@@ -352,19 +386,33 @@ pub enum PrefixedInstruction {
 }
 
 impl PrefixedInstruction {
-    pub fn from_opcode(opcode: u8) -> Self {
+    pub fn from(opcode: u8) -> Self {
         match opcode {
-            x if (x & 0b1111_1000) == 0b0000_0000 => Self::RotateLeft(r8(x, 0)),
-            x if (x & 0b1111_1000) == 0b0000_1000 => Self::RotateRight(r8(x, 0)),
-            x if (x & 0b1111_1000) == 0b0001_0000 => Self::RotateLeftThroughCarry(r8(x, 0)),
-            x if (x & 0b1111_1000) == 0b0001_1000 => Self::RotateRightThroughCarry(r8(x, 0)),
-            x if (x & 0b1111_1000) == 0b0010_0000 => Self::ShiftLeftArithmetic(r8(x, 0)),
-            x if (x & 0b1111_1000) == 0b0010_1000 => Self::ShiftRightArithmetic(r8(x, 0)),
-            x if (x & 0b1111_1000) == 0b0011_0000 => Self::Swap(r8(x, 0)),
-            x if (x & 0b1111_1000) == 0b0011_1000 => Self::ShiftRightLogical(r8(x, 0)),
-            x if (x & 0b1100_0000) == 0b0100_0000 => Self::CheckBit(r8(x, 0), u3(x, 3)),
-            x if (x & 0b1100_0000) == 0b1000_0000 => Self::ResetBit(r8(x, 0), u3(x, 3)),
-            x if (x & 0b1100_0000) == 0b1100_0000 => Self::SetBit(r8(x, 0), u3(x, 3)),
+            x if (x & 0b1111_1000) == 0b0000_0000 => Self::RotateLeft(Register8::from(x, 0)),
+            x if (x & 0b1111_1000) == 0b0000_1000 => Self::RotateRight(Register8::from(x, 0)),
+            x if (x & 0b1111_1000) == 0b0001_0000 => {
+                Self::RotateLeftThroughCarry(Register8::from(x, 0))
+            }
+            x if (x & 0b1111_1000) == 0b0001_1000 => {
+                Self::RotateRightThroughCarry(Register8::from(x, 0))
+            }
+            x if (x & 0b1111_1000) == 0b0010_0000 => {
+                Self::ShiftLeftArithmetic(Register8::from(x, 0))
+            }
+            x if (x & 0b1111_1000) == 0b0010_1000 => {
+                Self::ShiftRightArithmetic(Register8::from(x, 0))
+            }
+            x if (x & 0b1111_1000) == 0b0011_0000 => Self::Swap(Register8::from(x, 0)),
+            x if (x & 0b1111_1000) == 0b0011_1000 => Self::ShiftRightLogical(Register8::from(x, 0)),
+            x if (x & 0b1100_0000) == 0b0100_0000 => {
+                Self::CheckBit(Register8::from(x, 0), U3::from(x, 3))
+            }
+            x if (x & 0b1100_0000) == 0b1000_0000 => {
+                Self::ResetBit(Register8::from(x, 0), U3::from(x, 3))
+            }
+            x if (x & 0b1100_0000) == 0b1100_0000 => {
+                Self::SetBit(Register8::from(x, 0), U3::from(x, 3))
+            }
             _ => unreachable!(),
         }
     }
