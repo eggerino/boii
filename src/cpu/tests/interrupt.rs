ab4fe15135bcf2aa2dd0cc3e4006216913d00575
eggerino <eggensberger.armin@googlemail.com>
2026-08-03 19:32:33 +0000

    more interrupt tests

--- src/cpu/tests/interrupt.rs
+++ src/cpu/tests/interrupt.rs
@@ -76,3 +76,68 @@ fn enable_and_disable_interrupt() {
     cpu.step().unwrap();
     assert_cpu(10, state(0, 0, 0, 0, 0, 0x010A, false, false), &cpu);
 }
+
+#[test]
+fn interrupt_calls() {
+    let data = [
+        (0b0000_0001, 0x0040),
+        (0b0000_0010, 0x0048),
+        (0b0000_0100, 0x0050),
+        (0b0000_1000, 0x0058),
+        (0b0001_0000, 0x0060),
+    ];
+
+    for (interrupt_mask, target_address) in data {
+        let mut bus = prog([]);
+        ensure_size(&mut bus, 0x1_0000);
+
+        bus.write(0xFF0F, interrupt_mask).unwrap(); // Prepare the interrupt
+        bus.write(0xFFFF, interrupt_mask).unwrap();
+        bus.write(target_address, 0b0000_0100).unwrap(); // inc b in handler
+
+        let mut cpu = cpu(bus, state(0, 0, 0, 0, 2, 0x0100, false, true));
+
+        step(&mut cpu, 2);
+
+        assert_cpu(
+            6,
+            state(0, 0x0100, 0, 0, 0, target_address + 1, false, false),
+            &cpu,
+        );
+        assert_eq!(cpu.bus[0xFF0F], 0);
+        assert_eq!(cpu.bus[0], 0x00);
+        assert_eq!(cpu.bus[1], 0x01);
+    }
+}
+
+#[test]
+fn interrupt_priority() {
+    let mut bus = prog([0b1111_1011]); // ei
+    ensure_size(&mut bus, 0x1_0000);
+
+    bus.write(0xFF0F, 0b0001_1111).unwrap(); // prepare all interrupts
+    bus.write(0xFFFF, 0b0001_1111).unwrap();
+    bus.write(0x40, 0b1111_1011).unwrap(); // ie all handlers
+    bus.write(0x48, 0b1111_1011).unwrap(); // ie all handlers
+    bus.write(0x50, 0b1111_1011).unwrap(); // ie all handlers
+    bus.write(0x58, 0b1111_1011).unwrap(); // ie all handlers
+    bus.write(0x60, 0b1111_1011).unwrap(); // ie all handlers
+
+    let mut cpu = cpu(bus, state(0, 0, 0, 0, 10, 0x0100, false, false));
+
+    step(&mut cpu, 3);
+    assert_eq!(cpu.reg.prog_counter, 0x0040);
+    assert_eq!(cpu.bus[0xFF0F], 0b0001_1110);
+    step(&mut cpu, 3);
+    assert_eq!(cpu.reg.prog_counter, 0x0048);
+    assert_eq!(cpu.bus[0xFF0F], 0b0001_1100);
+    step(&mut cpu, 3);
+    assert_eq!(cpu.reg.prog_counter, 0x0050);
+    assert_eq!(cpu.bus[0xFF0F], 0b0001_1000);
+    step(&mut cpu, 3);
+    assert_eq!(cpu.reg.prog_counter, 0x0058);
+    assert_eq!(cpu.bus[0xFF0F], 0b0001_0000);
+    step(&mut cpu, 3);
+    assert_eq!(cpu.reg.prog_counter, 0x0060);
+    assert_eq!(cpu.bus[0xFF0F], 0b0000_0000);
+}
